2a11e46a47a1b3b5397a505c0f0ea9e4cf58d671
Max Jiam <mjim259@gmail.com>
2020-11-01 13:17:44 -0700

    login standin

--- dashboard/lib/main.dart
+++ dashboard/lib/main.dart
@@ -1,74 +1,21 @@
-import 'package:dashboard/hackathon/hack.dart';
-import 'package:dashboard/info/info.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
+import 'login.dart';
 
-import 'package:dashboard/account/acc.dart';
+final GlobalKey<NavigatorState> key = new GlobalKey<NavigatorState>();
 
 void main() {
-  runApp(MyApp());
+  runApp(FirstUp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+class FirstUp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  List<Widget> _widgetOptions = <Widget>[
-    HackHome(),
-    InfoHome(),
-    AccountHome(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.code),
-            label: "Hackathon"
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: "Information"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Account"
-          )
-        ],
-        selectedItemColor: Colors.red,
-        currentIndex: _selectedIndex,
-        onTap: (index) => setState(
-          () {
-            _selectedIndex = index;
-          } 
-        ),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  return MaterialApp(
+    home: LogIn(),
+    navigatorKey: key,
+    routes: <String, WidgetBuilder>{
+      "/app": (context) => App(),
+    },
+  );
   }
 }
