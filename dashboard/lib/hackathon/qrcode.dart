28ee1e54cbf0d93cbeb657687afdf184c05741a0
Joyce Hong <joyce.hong8@gmail.com>
2021-01-09 23:45:36 -0500

    Added comments, timestamps, expandable details for event objects Also rotation lock, QR loading indication, slight InfoTile changes

--- dashboard/lib/hackathon/qrcode.dart
+++ dashboard/lib/hackathon/qrcode.dart
@@ -4,6 +4,8 @@ import 'package:qrscan/qrscan.dart' as scanner;
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 
 void main() {
@@ -21,21 +23,22 @@ class HistoryItem{
   String text1;
   String text2;
   String text3;
+  String comment;
   
-  HistoryItem(this.text1, this.text2, this.text3);
+  HistoryItem(this.text1, this.text2, this.text3, this.comment);
 }
 
 
 class _QRHomeState extends State<QRHome> {
 
   List history = new List();
-  List scanConfig = ["One", "One", false];
+  List scanConfig = ["One", "One", false, ""];
   String id;
   bool admin = false;
 
-  void addHistory(text1, text2, text3){
+  void addHistory(text1, text2, text3, comment){
     setState(() {
-      history.add(new HistoryItem(text1, text2, text3));
+      history.insert(0, new HistoryItem(text1, text2, text3, comment));
     });
   }
 
@@ -78,11 +81,13 @@ class _QRHomeState extends State<QRHome> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
         title: 'QR Scanner',
         theme: ThemeData(
           canvasColor: Colors.white,
-          accentColor: Color(0xffcb1a1d),
+          primaryColor: Color(0xffcb1a1d),
+          accentColor: Colors.blue,
           buttonColor: Colors.black,
           fontFamily: 'Lato',
           textTheme: TextTheme(
@@ -95,7 +100,7 @@ class _QRHomeState extends State<QRHome> {
       ),
         home: QRPage(history: history, addHistory: addHistory,
           delHistory: delHistory, scanConfig: scanConfig, setConfig: setConfig,
-          setID: setID, id: id, admin: admin,)
+          setID: setID, id: id, admin: admin)
     );
   }
 }
@@ -125,7 +130,8 @@ class QRPage extends StatelessWidget{
                 delHistory: delHistory, editing: true)),
       );
     }else {
-      addHistory(scanRes, scanConfig[0], scanConfig[1]);
+      addHistory(scanConfig[0], DateFormat.jm().add_yMd() .format(DateTime.now()),
+          scanRes, scanConfig[3]);
     }
   }
 
@@ -135,7 +141,7 @@ class QRPage extends StatelessWidget{
         appBar: AppBar(
           title: Text('Your QR Code',
               style: Theme.of(context).textTheme.headline1),
-          backgroundColor: Theme.of(context).accentColor,
+          backgroundColor: Theme.of(context).primaryColor,
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.admin_panel_settings, size: 30),
@@ -150,11 +156,25 @@ class QRPage extends StatelessWidget{
             child: Column(
                 children: <Widget>[
                   const SizedBox(height: 30),
-                  QrImage(
-                    data: "$id",
-                    version: QrVersions.auto,
-                    size: 300.0,
-                    foregroundColor: (id == null) ? Colors.white : Colors.black,
+                  SizedBox(
+                    height: 300,
+                    width: 300,
+                    child: (id != null) ? QrImage(
+                      data: "$id",
+                      version: QrVersions.auto,
+                      size: 300.0,
+                      foregroundColor: Colors.black,
+                    )
+                    : Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Loading...",
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        )
+                      )
+                    )
                   ),
                   const SizedBox(height: 20),
                   RaisedButton(
@@ -218,52 +238,107 @@ class InfoTile extends StatelessWidget{
   Widget build(BuildContext context){
     return Card(
         margin: const EdgeInsets.all(12),
-        child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Row(
-              children: [
-                if(editing)
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () {
-                            delHistory(info);
-                          }
+        child: InkWell(
+          onTap: () async {
+            return showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(
+                      '${info.text1}',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       )
-                    ],
                   ),
-                  const SizedBox(width: 20),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: <Widget>[
+                        Text(
+                            '${info.text2}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey[700],
+                            )
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                            '${info.text3}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey[700],
+                            )
+                        ),
+                        const SizedBox(height: 14),
+                        Text(
+                          '${info.comment}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[500],
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('Close'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+            child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Row(
                     children: [
-                      Text(
-                          '${info.text1}',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          )
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                          '${info.text2}',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey[500],
-                          )
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                          '${info.text3}',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey[500],
-                          )
-                      ),
+                      if(editing)
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  delHistory(info);
+                                }
+                            )
+                          ],
+                        ),
+                      const SizedBox(width: 20),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                '${info.text1}',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                )
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                                '${info.text2}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey[500],
+                                )
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                                '${info.text3}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey[500],
+                                )
+                            ),
+                          ]
+                      )
                     ]
                 )
-              ]
             )
         )
     );
@@ -286,7 +361,7 @@ class HistoryPage extends StatelessWidget{
         appBar: AppBar(
           title: Text('Scan History',
               style: Theme.of(context).textTheme.headline1),
-          backgroundColor: Theme.of(context).accentColor,
+          backgroundColor: Theme.of(context).primaryColor,
           toolbarHeight: 70,
         ),
         body: Column(
@@ -306,13 +381,32 @@ class HistoryPage extends StatelessWidget{
   }
 }
 
-
-class ConfigPage extends StatelessWidget {
-
+class ConfigPage extends StatefulWidget{
   final List scanConfig;
   final Function setConfig;
 
   ConfigPage({this.scanConfig, this.setConfig});
+
+  _ConfigPageState createState() => _ConfigPageState();
+}
+
+
+class _ConfigPageState extends State<ConfigPage> {
+
+  final commentControl = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    commentControl.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    commentControl.text = !widget.scanConfig[2] ? widget.scanConfig[3] : "";
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -320,85 +414,121 @@ class ConfigPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Scan Config',
               style: Theme.of(context).textTheme.headline1),
-          backgroundColor: Theme.of(context).accentColor,
+          backgroundColor: Theme.of(context).primaryColor,
           toolbarHeight: 70,
         ),
         body:Padding(
             padding: const EdgeInsets.all(24),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                      children: [
-                        Container(
-                          child: Text("Option A",
-                              style: Theme.of(context).textTheme.subtitle1),
-                          width: 100,
-                        ),
-                        const SizedBox(width: 50),
-                        DropdownButton<String>(
-                            value: scanConfig[0],
-                            items: <String>['One', 'Two', 'Three', 'Four']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Container(
-                                    child: Text(value),
-                                    width: 100
-                                )
-                              );
-                            }).toList(),
-                            disabledHint: Text(scanConfig[0]),
-                            underline: Container(
-                                height: 2,
-                                color: Theme.of(context).accentColor
-                            ),
-                            onChanged: (!scanConfig[2]) ? (String newValue) {
-                              setConfig(newValue, 0);
-                            } : null
-                        ),
-                      ]
-                  ),
-                  Row(
-                      children:[
-                        Container(
-                          child: Text("Option A",
-                              style: Theme.of(context).textTheme.subtitle1),
-                          width: 100,
-                        ),
-                        const SizedBox(width: 50),
-                        DropdownButton<String>(
-                            value: scanConfig[1],
-                            items: <String>['One', 'Two', 'Three', 'Four']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Container(
-                                  child: Text(value),
-                                  width: 100
-                                )
-                              );
-                            }).toList(),
-                            disabledHint: Text(scanConfig[1]),
-                            underline: Container(
-                              height: 2,
-                              color: Theme.of(context).accentColor
-                            ),
-                            onChanged: (!scanConfig[2]) ? (String newValue) {
-                              setConfig(newValue, 1);
-                            } : null
-                        )
-                      ]
-                  ),
-                  const SizedBox(height:20),
-                  CheckboxListTile(
-                    title: Text("Edit Mode"),
-                    value: scanConfig[2],
-                    onChanged: (bool newValue) {
-                      setConfig(newValue, 2);
-                    }
-                  )
-                ]
+            child: SingleChildScrollView(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                        children: [
+                          Container(
+                            child: Text("Option A",
+                                style: Theme.of(context).textTheme.subtitle1),
+                            width: 120,
+                          ),
+                          const SizedBox(width: 50),
+                          Expanded(
+                            child: DropdownButton<String>(
+                                isExpanded: true,
+                                value: widget.scanConfig[0],
+                                items: <String>['One', 'Two', 'Three', 'Four']
+                                    .map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value)
+                                  );
+                                }).toList(),
+                                disabledHint: Text(widget.scanConfig[0]),
+                                underline: Container(
+                                    height: 2,
+                                    color: (!widget.scanConfig[2]) ?
+                                    Theme.of(context).primaryColor
+                                        : Colors.grey[500]
+                                ),
+                                onChanged: (!widget.scanConfig[2]) ? (String newValue) {
+                                  widget.setConfig(newValue, 0);
+                                } : null
+                            )
+                          )
+                        ]
+                    ),
+                    Row(
+                        children:[
+                          Container(
+                            child: Text("Option B",
+                                style: Theme.of(context).textTheme.subtitle1),
+                            width: 120,
+                          ),
+                          const SizedBox(width: 50),
+                          Expanded(
+                              child: DropdownButton<String>(
+                                  isExpanded: true,
+                                  value: widget.scanConfig[1],
+                                  items: <String>['One', 'Two', 'Three', 'Four']
+                                      .map<DropdownMenuItem<String>>((String value) {
+                                    return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                    );
+                                  }).toList(),
+                                  disabledHint: Text(widget.scanConfig[1]),
+                                  underline: Container(
+                                      height: 2,
+                                      color: (!widget.scanConfig[2]) ?
+                                      Theme.of(context).primaryColor
+                                          : Colors.grey[500]
+                                  ),
+                                  onChanged: (!widget.scanConfig[2]) ? (String newValue) {
+                                    widget.setConfig(newValue, 1);
+                                  } : null
+                              )
+                          )
+                        ]
+                    ),
+                    const SizedBox(height:20),
+                    TextField(
+                      autofocus: false,
+                      enabled: !widget.scanConfig[2],
+                      controller: commentControl,
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: !widget.scanConfig[2] ? "Additional comment"
+                            : "No comments in delete mode"
+                      ),
+                      onChanged: (String value) {
+                        widget.setConfig(value, 3);
+                      },
+                    ),
+                    const SizedBox(height:20),
+                    CheckboxListTile(
+                      title: Text("Delete Mode"),
+                      value: widget.scanConfig[2],
+                      onChanged: (bool newValue) {
+                        if(newValue){
+                          commentControl.clear();
+                        }else{
+                          commentControl.text = widget.scanConfig[3];
+                        }
+                        widget.setConfig(newValue, 2);
+                      }
+                    ),
+                    const SizedBox(height:20),
+                    RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      padding: const EdgeInsets.only(top:10, bottom:10,
+                          left:60, right:60),
+                      child: Text('Confirm',
+                          style: Theme.of(context).textTheme.button),
+                    )
+                  ]
+              )
             )
         )
     );
