4d1e53e476f5d0fc8f179c76560a95048e0fe8fa
Shreeya Khurana <khurana.shreeya@gmail.com>
2021-01-14 14:56:18 -0500

    initial form testing

--- dashboard/lib/hackathon/hack.dart
+++ dashboard/lib/hackathon/hack.dart
@@ -1,12 +1,88 @@
 import 'package:flutter/material.dart';
 
-class HackHome extends StatelessWidget {
+class HackHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormScreenState();
+  }
+}
+class FormScreenState extends State<HackHome> {
+
+  String _teamName;
+  String _url;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Widget _buildName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Team Name'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _teamName = value;
+      },
+    );
+  }
+
+  Widget _builURL() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Project Url'),
+      keyboardType: TextInputType.url,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'URL is Required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _url = value;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Tap here to just access the app. Dev only."),
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red, title: Text("Project Submission Form")),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildName(),
+                _builURL(),
+                SizedBox(height: 100),
+                RaisedButton(
+                  child: Text(
+                    'Send',
+                    style: TextStyle(color: Colors.red, fontSize: 16),
+                  ),
+                  onPressed: () {
+                    if (!_formKey.currentState.validate()) {
+                      return;
+                    }
+
+                    _formKey.currentState.save();
+
+                    print(_teamName);
+                    print(_url);
+
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
   
 }
+
