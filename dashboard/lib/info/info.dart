02fff7d3c5d8dea4ef425382548c3114200ee005
Max Jiam <mjim259@gmail.com>
2021-02-23 16:26:37 -0500

    moved scheduling portion into info, updated with medha's code

--- dashboard/lib/info/info.dart
+++ dashboard/lib/info/info.dart
@@ -1,10 +1,11 @@
+import 'package:dashboard/info/scheduling.dart';
 import 'package:flutter/material.dart';
 
 class InfoHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DynamicSchedule();
   }
   
 }
